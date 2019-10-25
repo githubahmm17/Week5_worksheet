a8d8f8d99b69aaefc95ca232d8ee2b394f054ce1
githubahmm17 <55874247+githubahmm17@users.noreply.github.com>
2019-10-25 09:57:49 +0100

    changes to js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
-
 document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-
-
 
 // device APIs are available
 //
