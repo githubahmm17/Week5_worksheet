0c508a4f329bc25b383f905c449374ac25517d08
githubahmm17 <55874247+githubahmm17@users.noreply.github.com>
2019-10-25 09:54:48 +0100

    changes to code

--- js/index.js
+++ js/index.js
@@ -5,19 +5,12 @@ var launched_count = 0;
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
 
-}
 
 
 // device APIs are available
 //
     function onDeviceReady() {
-	
-
-	launched_count++;
-	updateDisplay();
 	    
 	alert("device ready");
     }
